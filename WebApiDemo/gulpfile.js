d61a019ccb6e290d460732e29af7866b7b839890
JarkkoKaa <jarkko.kaartinen@edu.savonia.fi>
2018-09-19 09:37:04 +0300

    CSS added

--- WebApiDemo/gulpfile.js
+++ WebApiDemo/gulpfile.js
@@ -4,12 +4,17 @@ var del = require('del');
 
 var paths = {
     scripts: ['Source/**/*.js', 'Source/**/*.ts', 'Source/**/*.map'],
+    styles: ['Source/**/*.css'],
 };
 
 gulp.task('clean', function () {
     return del(['wwwroot/js/**/*']);
 });
+gulp.task('clean', function () {
+    return del(['wwwroot/css/**/*']);
+});
 
 gulp.task('default', function () {
     gulp.src(paths.scripts).pipe(gulp.dest('wwwroot/js'));
+    gulp.src(paths.styles).pipe(gulp.dest('wwwroot/css'));
 });
